b39bf56bde2aa6f37a6dc339fd9a5180201b7793
piccioli <piccioli@netseven.it>
2020-05-27 06:58:59 +0200

    Added config.json save in install script (DOCUMENT ROOT must be changed)

--- scripts/install_prod.php
+++ scripts/install_prod.php
@@ -21,8 +21,24 @@ else {
         die();
     }
     // Proceed
+
+    // Save config file (if exists)
+    $has_conf = false;
+    $conf_src = "$path/src/config.json";
+    if(file_exists($conf_src)) {
+        $has_conf=true;
+        $conf_tmp = tempnam('/tmp','hoquconf_');
+        $cmd = "cp $conf_src $conf_tmp";
+        system($cmd);
+    }
+
     $cmd = "rm -rf $path/*"; system($cmd);
-    $cmd = "cp -R ".__DIR__."/../src/* $path"; system($cmd);
+    $cmd = "cp -R ".__DIR__."/../* $path"; system($cmd);
+    if ($has_conf) {
+        $cmd = "cp $conf_tmp $conf_src";
+        system($cmd);
+        $cmd = "rm $conf_tmp";
+    }
     echo "Software HOQU installed in $path ... ALL DONE\n\n";
 }
 
